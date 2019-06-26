0e6ebcbf9e04e2fa98f0e53661944475e3682d3c
VyacheslavAI <vyacheslavivanov93@gmail.com>
2019-06-26 13:05:30 +0300

    Доработать сохранение переданного сообщения в БД

--- receiver/src/main/java/ru/iteco/vyacheslav/listener/Listener.java
+++ receiver/src/main/java/ru/iteco/vyacheslav/listener/Listener.java
@@ -1,13 +1,22 @@
 package ru.iteco.vyacheslav.listener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+import ru.iteco.vyacheslav.service.api.IStringHolderService;
 
 @Component
 public class Listener {
 
+    private IStringHolderService stringHolderService;
+
+    @Autowired
+    public Listener(IStringHolderService stringHolderService) {
+        this.stringHolderService = stringHolderService;
+    }
+
     @JmsListener(destination = "box")
-    public void listen(String string) {
-        System.out.println(string);
+    public void listen(String message) {
+        stringHolderService.createOrUpdateStringHolder(message);
     }
 }
